d30a19f313060fac42f6dd71ed57815c0c992ba8
Kurihara Jun <lh182051+src@gmail.com>
2021-06-02 21:14:45 +0900

    ✨ Add feature for parse coverage file
    This commit is to add parser class which parse xml file
    of covetura coverrage repot format.

---
+++ __tests__/CobeturaParser.test.ts
@@ -0,0 +1,217 @@
+import { mocked } from 'ts-jest/utils'
+import { CobeturaParser } from '../src/parser/CobeturaParser'
+import fs from 'fs'
+import * as core from '@actions/core'
+
+jest.mock('fs')
+jest.mock('@actions/core')
+
+const fsExists = mocked(fs.existsSync)
+const fsReadSync = mocked(fs.readFileSync)
+const coreDebug = mocked(core.debug)
+afterEach(() => {
+  fsExists.mockClear()
+  fsReadSync.mockClear()
+  coreDebug.mockClear()
+})
+
+describe('Test CobeturaParser', () => {
+  it('File does not exist return null', () => {
+    fsExists.mockImplementation(_ => false)
+    const target = new CobeturaParser()
+    const actual = target.parse('sample.xml')
+    expect(actual).toBeUndefined()
+  })
+
+  it('Multiple packages and many classes, methods', () => {
+    fsExists.mockImplementation(_ => true)
+    fsReadSync.mockImplementation(_path => {
+      return Buffer.from(`<?xml version="1.0" encoding="utf-8"?>
+      <coverage line-rate="0.8181" branch-rate="0.75" version="1.9" timestamp="1622395212" lines-covered="9" lines-valid="11" branches-covered="3" branches-valid="4">
+        <sources>
+          <source>/</source>
+        </sources>
+        <packages>
+          <package name="Sample" line-rate="0.8181" branch-rate="0.75" complexity="4">
+            <classes>
+              <class name="Sample.Class1" filename="Users/kuriharajun/projects/github/coverage-comment-test/csharp/src/Sample/Class1.cs" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                <methods>
+                  <method name="ToConvertInt" signature="(System.String)" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                    <lines>
+                      <line number="6" hits="3" branch="False" />
+                      <line number="7" hits="3" branch="True" condition-coverage="100% (2/2)">
+                        <conditions>
+                          <condition number="9" type="jump" coverage="100%" />
+                        </conditions>
+                      </line>
+                    </lines>
+                  </method>
+                  <method name="ToConvertFloat" signature="(System.String)" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                    <lines>
+                      <line number="6" hits="3" branch="False" />
+                      <line number="7" hits="3" branch="True" condition-coverage="100% (2/2)">
+                        <conditions>
+                          <condition number="9" type="jump" coverage="100%" />
+                        </conditions>
+                      </line>
+                    </lines>
+                  </method>
+                </methods>
+              </class>
+              <class name="Sample.Class2" filename="Users/kuriharajun/projects/github/coverage-comment-test/csharp/src/Sample/Class1.cs" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                <methods>
+                  <method name="ToConvertInt" signature="(System.String)" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                    <lines>
+                      <line number="6" hits="3" branch="False" />
+                      <line number="7" hits="3" branch="True" condition-coverage="100% (2/2)">
+                        <conditions>
+                          <condition number="9" type="jump" coverage="100%" />
+                        </conditions>
+                      </line>
+                    </lines>
+                  </method>
+                </methods>
+              </class>
+            </classes>
+          </package>
+          <package name="Sample.V2" line-rate="0.8181" branch-rate="0.75" complexity="4">
+          </package>
+        </packages>
+      </coverage>`)
+    })
+    const target = new CobeturaParser()
+    const actual = target.parse('sample.xml')
+    expect(coreDebug.mock.calls[0]).toEqual(['Array'])
+    expect(actual).toStrictEqual({
+      lineRate: 0.8181,
+      branchRate: 0.75,
+      lineCovered: 9,
+      lineValid: 11,
+      branchCovered: 3,
+      branchValid: 4,
+      objectCoverages: [
+        {
+          name: 'Sample',
+          lineRate: 0.8181,
+          branchRate: 0.75,
+          complexity: 4,
+          objectCoverages: [
+            {
+              name: 'Sample.Class1',
+              lineRate: 0.8181,
+              branchRate: 0.75,
+              complexity: 4,
+              methodCoverages: [
+                {
+                  name: 'ToConvertInt',
+                  signature: '(System.String)',
+                  lineRate: 0.8181,
+                  branchRate: 0.75,
+                  complexity: 4
+                },
+                {
+                  name: 'ToConvertFloat',
+                  signature: '(System.String)',
+                  lineRate: 0.8181,
+                  branchRate: 0.75,
+                  complexity: 4
+                }
+              ]
+            },
+            {
+              name: 'Sample.Class2',
+              lineRate: 0.8181,
+              branchRate: 0.75,
+              complexity: 4,
+              methodCoverages: [
+                {
+                  name: 'ToConvertInt',
+                  signature: '(System.String)',
+                  lineRate: 0.8181,
+                  branchRate: 0.75,
+                  complexity: 4
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'Sample.V2',
+          lineRate: 0.8181,
+          branchRate: 0.75,
+          complexity: 4,
+          objectCoverages: undefined
+        }
+      ]
+    })
+    console.log(coreDebug.mock.calls[1])
+  })
+  it('Single package and single class, method', () => {
+    fsExists.mockImplementation(_ => true)
+    fsReadSync.mockImplementation(_path => {
+      return Buffer.from(`<?xml version="1.0" encoding="utf-8"?>
+      <coverage line-rate="0.8181" branch-rate="0.75" version="1.9" timestamp="1622395212" lines-covered="9" lines-valid="11" branches-covered="3" branches-valid="4">
+        <sources>
+          <source>/</source>
+        </sources>
+        <packages>
+          <package name="Sample" line-rate="0.8181" branch-rate="0.75" complexity="4">
+            <classes>
+              <class name="Sample.Class1" filename="Users/kuriharajun/projects/github/coverage-comment-test/csharp/src/Sample/Class1.cs" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                <methods>
+                  <method name="ToConvertInt" signature="(System.String)" line-rate="0.8181" branch-rate="0.75" complexity="4">
+                    <lines>
+                      <line number="6" hits="3" branch="False" />
+                      <line number="7" hits="3" branch="True" condition-coverage="100% (2/2)">
+                        <conditions>
+                          <condition number="9" type="jump" coverage="100%" />
+                        </conditions>
+                      </line>
+                    </lines>
+                  </method>
+                </methods>
+              </class>
+            </classes>
+          </package>
+        </packages>
+      </coverage>`)
+    })
+    const target = new CobeturaParser()
+    const actual = target.parse('sample.xml')
+    expect(coreDebug.mock.calls[0]).toEqual(['Object'])
+    expect(actual).toStrictEqual({
+      lineRate: 0.8181,
+      branchRate: 0.75,
+      lineCovered: 9,
+      lineValid: 11,
+      branchCovered: 3,
+      branchValid: 4,
+      objectCoverages: [
+        {
+          name: 'Sample',
+          lineRate: 0.8181,
+          branchRate: 0.75,
+          complexity: 4,
+          objectCoverages: [
+            {
+              name: 'Sample.Class1',
+              lineRate: 0.8181,
+              branchRate: 0.75,
+              complexity: 4,
+              methodCoverages: [
+                {
+                  name: 'ToConvertInt',
+                  signature: '(System.String)',
+                  lineRate: 0.8181,
+                  branchRate: 0.75,
+                  complexity: 4
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    })
+    console.log(coreDebug.mock.calls[1])
+  })
+})
---
+++ src/parser/CobeturaParser.ts
@@ -0,0 +1,131 @@
+import fs from 'fs'
+import parser from 'fast-xml-parser'
+import {CoverageEntity, IParser, MethodEntity, ObjectEntity} from './IParser'
+import * as core from '@actions/core'
+
+const nameAttr = '@_name'
+const lineRateAttr = '@_line-rate'
+const branchRateAttr = '@_branch-rate'
+const linesCoveredAttr = '@_lines-covered'
+const linesValidAttr = '@_lines-valid'
+const branchesCoveredAttr = '@_branches-covered'
+const branchesValidAttr = '@_branches-valid'
+const complexityAttr = '@_complexity'
+const signatureAttr = '@_signature'
+
+export class CobeturaParser implements IParser {
+  parse(path: string): CoverageEntity | undefined {
+    if (!fs.existsSync(path)) {
+      return
+    }
+    const coverageXml = parser.parse(fs.readFileSync(path).toString('utf-8'), {
+      attributeNamePrefix: '@_',
+      attrNodeName: 'attr',
+      textNodeName: '#text',
+      ignoreAttributes: false,
+      parseNodeValue: true,
+      parseAttributeValue: true,
+      trimValues: true
+    })
+    coverageXml.coverage.packages
+    const packages = this.parsePackage(coverageXml.coverage.packages.package)
+    return {
+      lineRate: coverageXml.coverage.attr[lineRateAttr],
+      branchRate: coverageXml.coverage.attr[branchRateAttr],
+      lineCovered: coverageXml.coverage.attr[linesCoveredAttr],
+      lineValid: coverageXml.coverage.attr[linesValidAttr],
+      branchCovered: coverageXml.coverage.attr[branchesCoveredAttr],
+      branchValid: coverageXml.coverage.attr[branchesValidAttr],
+      objectCoverages: packages
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private parsePackage(coverage: any): ObjectEntity[] | undefined {
+    if (coverage instanceof Array) {
+      core.debug('Array')
+      return coverage.map(v => {
+        let classCoverage: ObjectEntity[] | undefined = undefined
+        if (v.classes) {
+          classCoverage = this.parseClass(v.classes.class)
+        }
+        return {
+          name: v.attr[nameAttr],
+          lineRate: v.attr[lineRateAttr],
+          branchRate: v.attr[branchRateAttr],
+          complexity: v.attr[complexityAttr],
+          objectCoverages: classCoverage
+        }
+      })
+    } else {
+      core.debug('Object')
+      return [
+        {
+          name: coverage.attr[nameAttr],
+          lineRate: coverage.attr[lineRateAttr],
+          branchRate: coverage.attr[branchRateAttr],
+          complexity: coverage.attr[complexityAttr],
+          objectCoverages: this.parseClass(coverage.classes.class)
+        }
+      ]
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private parseClass(coverage: any): ObjectEntity[] | undefined {
+    if (coverage instanceof Array) {
+      return coverage.map(v => {
+        let methodEntities: MethodEntity[] | undefined = undefined
+        if (v.methods) {
+          methodEntities = this.parseMethod(v.methods.method)
+        }
+        return {
+          name: v.attr[nameAttr],
+          lineRate: v.attr[lineRateAttr],
+          branchRate: v.attr[branchRateAttr],
+          complexity: v.attr[complexityAttr],
+          methodCoverages: methodEntities
+        }
+      })
+    } else {
+      let methodEntities: MethodEntity[] | undefined = undefined
+      if (coverage.methods) {
+        methodEntities = this.parseMethod(coverage.methods.method)
+      }
+      return [
+        {
+          name: coverage.attr[nameAttr],
+          lineRate: coverage.attr[lineRateAttr],
+          branchRate: coverage.attr[branchRateAttr],
+          complexity: coverage.attr[complexityAttr],
+          methodCoverages: methodEntities
+        }
+      ]
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private parseMethod(coverage: any): MethodEntity[] | undefined {
+    if (coverage instanceof Array) {
+      return coverage.map(v => {
+        return {
+          name: v.attr[nameAttr],
+          signature: v.attr[signatureAttr],
+          lineRate: v.attr[lineRateAttr],
+          branchRate: v.attr[branchRateAttr],
+          complexity: v.attr[complexityAttr]
+        }
+      })
+    } else {
+      return [
+        {
+          name: coverage.attr[nameAttr],
+          signature: coverage.attr[signatureAttr],
+          lineRate: coverage.attr[lineRateAttr],
+          branchRate: coverage.attr[branchRateAttr],
+          complexity: coverage.attr[complexityAttr]
+        }
+      ]
+    }
+  }
+}
