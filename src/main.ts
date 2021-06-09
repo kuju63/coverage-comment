a65c275622eb4837a27bc2523da2684dac040fd3
Kurihara Jun <lh182051+src@gmail.com>
2021-06-09 19:13:27 +0900

    :construction: Add input parameter

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,51 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import glob from '@actions/glob'
+import {CoberturaParser} from './parser/CoberturaParser'
+
+const types = ['cobertura']
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const debug = core.getInput('debug') === '1'
+    const paths = core.getInput('paths', {required: true, trimWhitespace: true})
+    const type = core.getInput('type', {trimWhitespace: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    GuardCoverageType(type)
 
-    core.setOutput('time', new Date().toTimeString())
+    const globber = await glob.create(paths)
+    const parser = new CoberturaParser()
+    let totalModuleCount = 0
+    let totalLineCovered = 0.0
+    let totalBranchCovered = 0.0
+    for await (const file of globber.globGenerator()) {
+      const coverage = parser.parse(file)
+      if (coverage) {
+        totalLineCovered += coverage.lineCovered
+        totalBranchCovered += coverage.branchCovered * 100
+      }
+      totalModuleCount++
+    }
+    if (totalModuleCount > 0) {
+      core.debug(
+        `Total line covered ${(totalLineCovered / totalModuleCount) * 100}`
+      )
+      core.debug(
+        `Total branch covered ${(totalBranchCovered / totalModuleCount) * 100}`
+      )
+    } else {
+      core.setFailed('Coverage file does not found.')
+    }
+
+    core.debug(`Input path ${paths}`)
+    core.debug(`Input debug mode ${debug}`)
   } catch (error) {
     core.setFailed(error.message)
+  }
+}
+
+function GuardCoverageType(type: string): void {
+  if (types.includes(type)) {
+    core.setFailed('type input has invalid argument')
   }
 }
 
